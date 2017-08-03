2fe9b3cc4ecaabc51c6782fc8491ed953383ae89
mcxtzhang <mcxtzhang@163.com>
2017-08-03 18:06:47 +0800

    init

---
+++ index.android.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.hello}>Hello, World2222256666699009</Text>
+      </View>
+    )
+  }
+
+  componentWillMount() {
+    console.log("zxt----componentWillMount："+this.props.key1);
+  }
+}
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  hello: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+AppRegistry.registerComponent('AlyXiaoChaoAppProject', () => HelloWorld);
