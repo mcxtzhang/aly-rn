255e46231e3f85c7660cdc359a92daaa4041ed30
mcxtzhang <mcxtzhang@163.com>
2017-08-04 10:00:27 +0800

    [s设置页面UI ]

--- settings.js
+++ settings.js
@@ -12,6 +12,8 @@ import {
 } from 'react-native';
 
 export default class Settings extends React.Component {
+
+
     onPressClearCache() {
         console.log("You tapped the onPressClearCache!");
     }
@@ -19,20 +21,47 @@ export default class Settings extends React.Component {
     render() {
         return (
             <ScrollView>
-                <View style={styles.settingItem}>
-                    <Text style={styles.settingsText}>关于我们</Text>
+                <View style={styles.settingItem10}>
+                    <Text style={styles.settingsText}>账号</Text>
+                    <Text style={styles.settingsValue}>xaxc001</Text>
                 </View>
-                <TouchableOpacity style={styles.settingItem}  onPress={() => {this.onPressClearCache()}} >
+
+
+                <TouchableOpacity style={styles.settingItem10}>
+                    <Text style={styles.settingsText}>打印机设置</Text>
+                    <Image source={require('./img/icon_arrow.png')}></Image>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.settingItem10}>
+                    <Text style={styles.settingsText}>通知与消息音设置</Text>
+                    <Image source={require('./img/icon_arrow.png')}></Image>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.settingItem10}>
+                    <Text style={styles.settingsText}>关于我们</Text>
+                    <Image source={require('./img/icon_arrow.png')}></Image>
+                </TouchableOpacity>
+                <View style={styles.settingItem1}>
+                    <Text style={styles.settingsText}>版本号</Text>
+                    <Text style={styles.settingsValue}>V1.0.0</Text>
+                </View>
+                <TouchableOpacity style={styles.settingItem1} onPress={() => {
+                    this.onPressClearCache()
+                }}>
                     <Text style={styles.settingsText}>清理缓存</Text>
+                    <Text style={styles.settingsValue}>0.0KB</Text>
                 </TouchableOpacity>
 
 
-                <Image source={require('./img/logo.png')} style={{marginTop: 54}}/>
-                <Text style={{color: '#333333', fontSize: 20, marginTop: 20}}>俺来也商22222222家版</Text>
-                <Text style={{color: '#666666', fontSize: 14, marginTop: 2, flex: 1}}>版本：V2.0</Text>
-                <Text style={{color: '#999999', fontSize: 12}}>俺来也（上海）网络科技有限公司</Text>
-                <Text style={{color: '#999999', fontSize: 12, marginTop: 6, marginBottom: 20}}>Copyright@2014-2016
-                    IMC</Text>
+                <View style={styles.settingItem10}>
+                    <Text style={styles.settingsText}>客服电话</Text>
+                    <Text style={[styles.settingsValue, {color: '#4990E2'}]}>400-686-9933</Text>
+                </View>
+
+
+                <TouchableOpacity style={styles.settingItem10}>
+                    <Text style={styles.settingsText}>退出登录</Text>
+                </TouchableOpacity>
 
             </ScrollView>
         )
@@ -44,17 +73,35 @@ var styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    settingItem: {
-        marginTop: 15,
+    settingItem10: {
+        marginTop: 10,
         paddingLeft: 15,
-        height: 60,
+        paddingRight: 15,
+        height: 48,
         backgroundColor: '#ffffff',
-        justifyContent: 'center',//main
+        //justifyContent: 'center',//main
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    settingItem1: {
+        marginTop: 1,
+        paddingLeft: 15,
+        paddingRight: 15,
+        height: 48,
+        backgroundColor: '#ffffff',
+        //justifyContent: 'center',//main
+        alignItems: 'center',
+        flexDirection: 'row'
     },
     settingsText: {
-        fontSize: 16
-    }
-
+        fontSize: 14,
+        color: '#666666',
+        flex: 1
+    },
+    settingsValue: {
+        fontSize: 14,
+        color: '#333333',
+    },
 
 });
 
